536ef5fe06c40931707b7504621c52d4bb95c472
dibley1973 <dibley1973@yahoo.co.uk>
2016-09-01 06:41:06 +0100

    Tried to add unit test projectbut what a faff!

---
+++ Code/Shared/ORMS.Shared.SharedKernel.UnitTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ORMS.Shared.SharedKernel.UnitTests
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
--- Code/Shared/ORMS.Shared.SharedKernel.UnitTests/Properties/AssemblyInfo.cs
+++ Code/Shared/ORMS.Shared.SharedKernel.UnitTests/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ORMS.Shared.SharedKernel.UnitTests")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ORMS.Shared.SharedKernel.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c2c7a604-215a-4868-8564-e0ff51ba6bfe")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("6e92e743-21a7-47de-ae87-a5358a72d954")]

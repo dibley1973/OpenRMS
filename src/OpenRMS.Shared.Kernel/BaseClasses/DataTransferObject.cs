ac925a4142e0b6bbafceb0c57c5d5e4054a32c17
jaa <github@joseph.adjare.co.uk>
2016-10-08 21:05:50 +0100

    Add xunit unit test project

--- src/OpenRMS.Shared.Kernel/BaseClasses/DataTransferObject.cs
+++ src/OpenRMS.Shared.Kernel/BaseClasses/DataTransferObject.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace OpenRMS.Shared.Kernel.BaseClasses
 {
+    //TODO: JAA - Question purpose of a DTO abstract class
     public abstract class DataTransferObject
     {
         

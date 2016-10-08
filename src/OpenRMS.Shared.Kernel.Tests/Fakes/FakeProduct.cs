ecfc6da0161d19c90c5751be44a1147a13e55fd9
dibley1973 <dibley1973@yahoo.co.uk>
2016-10-08 19:39:10 +0100

    Added unit test project for Shared.Kernel, with one test class and one method, but cannot get tests to run.

---
+++ src/OpenRMS.Shared.Kernel.Tests/Fakes/FakeProduct.cs
@@ -0,0 +1,9 @@
+﻿using OpenRMS.Shared.Kernel.BaseClasses;
+
+namespace OpenRMS.Shared.Kernel.Tests.Fakes
+{
+    public class FakeProduct : Entity<int>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/OpenRMS.Shared.Kernel.Tests/Tests/Amplifiers/MaybeTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenRMS.Shared.Kernel.Amplifiers;
+
+namespace OpenRMS.Shared.Kernel.Tests.Tests.Amplifiers
+{
+    [TestClass]
+    public class HasValueTests
+    {
+        [TestMethod]
+        public void HasValue_AfterConstructionWithNoArgument_ReturnsFalse()
+        {
+            // ARRANGE
+            var maybe = new Maybe<Fakes.FakeProduct, int>();
+
+            // ACT
+            var actual = maybe.HasValue();
+
+            // ASSERT
+            actual.Should().Be(false);
+        }
+    }
+}

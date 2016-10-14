a8287528ba2a114301b37d5088c7c6cba61fd1be
johnc <johnc@DESKTOP-0KO67VH>
2016-10-14 23:57:28 +0100

    #7 implemented test coverage over the items controller

--- src/OpenRMS.Contexts.ItemManagement.ApplicationService.Tests/Controllers/ItemsControllerTests.cs
+++ src/OpenRMS.Contexts.ItemManagement.ApplicationService.Tests/Controllers/ItemsControllerTests.cs
@@ -1,66 +1,158 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenRMS.Contexts.ItemManagement.Domain.Interfaces;
-using OpenRMS.Contexts.ItemManagement.ApplicationService.Tests.Fakes;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using OpenRMS.Contexts.ItemManagement.Api.Controllers;
-using System.Linq;
-using FluentAssertions;
 using OpenRMS.Contexts.ItemManagement.ApplicationService.CommandStack.Commands;
-using OpenRMS.Shared.Kernel.Interfaces;
+using OpenRMS.Contexts.ItemManagement.ApplicationService.CommandStack.Handlers;
+using OpenRMS.Contexts.ItemManagement.ApplicationService.Models;
 using OpenRMS.Contexts.ItemManagement.Domain.Entities;
+using OpenRMS.Contexts.ItemManagement.Domain.Interfaces;
+using OpenRMS.Shared.Kernel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenRMS.Contexts.ItemManagement.ApplicationService.Tests.Controllers
 {
     [TestClass]
     public class ItemsControllerTests
     {
-        private ICommandHandler<CreateItemCommand, Item> _fakeCreateItemHandler;
-        private ICommandHandler<UpdateItemCommand> _fakeUpdateItemHandler;
-        private ICommandHandler<DeleteItemCommand> _fakeDeleteItemHandler;
-        private IItemRepository _fakeItemRepository;
+        private Mock<ICommandHandler<CreateItemCommand, Item>> _createItemCommandHandlerMock;
+        private Mock<ICommandHandler<UpdateItemCommand>> _updateItemCommandHandlerMock;
+        private Mock<ICommandHandler<DeleteItemCommand>> _deleteItemCommandHandlerMock;
+        private Mock<IItemRepository> _itemRepositoryMock;
         private ItemsController _controller;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            _fakeCreateItemHandler = new FakeCreateItemCommandHandler();
-            _fakeUpdateItemHandler = new FakeUpdateItemCommandHandler();
-            _fakeDeleteItemHandler = new FakeDeleteItemCommandHandler();
-            _fakeItemRepository = new FakeItemRepository();
+            var testItemToCreate = new Item(Guid.NewGuid(), "Test item", "Test description");
+
+            _createItemCommandHandlerMock = new Mock<ICommandHandler<CreateItemCommand, Item>>();
+            _createItemCommandHandlerMock.Setup(m => m.Execute(It.IsAny<CreateItemCommand>())).Returns(testItemToCreate);
+
+            _updateItemCommandHandlerMock = new Mock<ICommandHandler<UpdateItemCommand>>();
+
+            _deleteItemCommandHandlerMock = new Mock<ICommandHandler<DeleteItemCommand>>();
+
+            _itemRepositoryMock = new Mock<IItemRepository>();
 
             _controller = new ItemsController(
-                _fakeItemRepository, _fakeCreateItemHandler, _fakeUpdateItemHandler, _fakeDeleteItemHandler);
+                _itemRepositoryMock.Object,
+                _createItemCommandHandlerMock.Object, 
+                _updateItemCommandHandlerMock.Object, 
+                _deleteItemCommandHandlerMock.Object);
         }
 
         [TestMethod]
-        public void Delete_AfterCalling_CountOfItemsInRepositoryHasDecreasedByOne()
+        public void Delete_ExecutesTheDeleteCommandHandler()
         {
-            // ARRANGE
-            var idToDelete = FakeItems.Item3Id;
-            var countOfItemsBefore = _fakeItemRepository.GetAll().Count();
+            // ARRANGE            
 
             // ACT
-            _controller.Delete(idToDelete);
-            var countOfItemsAfter = _fakeItemRepository.GetAll().Count();
-            var actual = countOfItemsAfter == countOfItemsBefore - 1;
+            _controller.Delete(Guid.NewGuid());
 
             // ASSERT
-            actual.Should().BeTrue();
+            _deleteItemCommandHandlerMock.Verify(handler => handler.Execute(It.IsAny<DeleteItemCommand>()), Times.Once);
         }
 
         [TestMethod]
-        public void Delete_AfterCalling_DeletedItemInRepositoryIsNotAvailable()
+        public void Put_ExecutesTheUpdateCommandHandler()
         {
-            // ARRANGE
-            var idToDelete = FakeItems.Item3Id;
-            var existsBefore = _fakeItemRepository.GetForId(idToDelete);
+            // ARRANGE            
+            var updateModel = new UpdateItemModel()
+            {
+                Name = "Updated Item",
+                Description = "Updated item description"
+            };
 
             // ACT
-            _controller.Delete(idToDelete);
-            
+            _controller.Put(Guid.NewGuid(), updateModel);
 
             // ASSERT
+            _updateItemCommandHandlerMock.Verify(handler => handler.Execute(It.IsAny<UpdateItemCommand>()), Times.Once);
+        }
 
+        [TestMethod]
+        public void Post_ExecutesTheCreateCommandHandler()
+        {
+            // ARRANGE                        
+            var createModel = new CreateItemModel()
+            {
+                Name = "Item name",
+                Description = "Item description"
+            };
 
+            // ACT
+            _controller.Post(createModel);
+
+            // ASSERT
+            _createItemCommandHandlerMock.Verify(handler => handler.Execute(It.IsAny<CreateItemCommand>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void Post_ReturnsTheIdOfTheCreatedItem()
+        {
+            // ARRANGE            
+            var createdItem = new Item(Guid.NewGuid(), "Created item", "Created item description");
+            _createItemCommandHandlerMock.Setup(m => m.Execute(It.IsAny<CreateItemCommand>())).Returns(createdItem);
+            var createModel = new CreateItemModel()
+            {
+                Name = "Item name",
+                Description = "Item description"
+            };
+
+            // ACT
+            var createdItemId = _controller.Post(createModel);
+
+            // ASSERT
+            createdItemId.Should().Be(createdItem.Id);
+        }
+
+        [TestMethod]
+        public void Get_ReturnsAllItemsInTheRepository()
+        {
+            // ARRANGE            
+            var items = new List<Item>()
+            {
+                new Item(Guid.NewGuid(), "Item 1", "Item 1"),
+                new Item(Guid.NewGuid(), "Item 2", "Item 2"),
+                new Item(Guid.NewGuid(), "Item 3", "Item 3")
+            };
+            _itemRepositoryMock.Setup(m => m.GetAll()).Returns(items);
+
+            // ACT
+            var returnedItems = _controller.Get();
+
+            // ASSERT
+            returnedItems.Count().Should().Be(items.Count);
+            foreach(GetItemModel returnedItem in returnedItems)
+            {
+                Assert.IsTrue(items.Any(item => item.Id == returnedItem.Id));
+            }
+        }
+
+        [TestMethod]
+        public void GetForId_ReturnsTheExpectedItemFromTheRepository()
+        {
+            // ARRANGE            
+            var itemToFind = new Item(Guid.NewGuid(), "Item 2", "Item 2");            
+            var items = new List<Item>()
+            {
+                new Item(Guid.NewGuid(), "Item 1", "Item 1"),
+                itemToFind,
+                new Item(Guid.NewGuid(), "Item 3", "Item 3")
+            };
+            _itemRepositoryMock.Setup(m => m.GetForId(It.IsAny<Guid>())).Returns(itemToFind);
+
+            // ACT
+            var returnedItem = _controller.Get(itemToFind.Id);
+
+            // ASSERT
+            returnedItem.Should().NotBe(null);
+            returnedItem.Id.Should().Be(itemToFind.Id);
+            returnedItem.Name.Should().Be(itemToFind.Name);
+            returnedItem.Description.Should().Be(itemToFind.Description);
         }
     }
 }

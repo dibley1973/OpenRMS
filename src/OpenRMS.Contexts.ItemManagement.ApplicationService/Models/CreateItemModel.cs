07b79d36f9375633c2238ea8ada41b5d2014ac1e
johnc <johnc@DESKTOP-0KO67VH>
2016-10-14 20:57:12 +0100

    #22 Removed duplicate service and added models for the rest app service

---
+++ src/OpenRMS.Contexts.ItemManagement.ApplicationService/Models/CreateItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenRMS.Contexts.ItemManagement.ApplicationService.Models
+{
+    public class CreateItemModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

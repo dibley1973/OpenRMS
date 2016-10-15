e09eb3b53d8554f5c5fdc513524f7b4d025d3d88
dibley1973 <dibley1973@yahoo.co.uk>
2016-10-15 06:14:20 +0100

    Added `Id_AfterClassInstantiation_ReturnsCorrectValue` tests to `DeleteItemCommandTests`

---
+++ src/OpenRMS.Contexts.ItemManagement.ApplicationService.Tests/CommandStack/Commands/DeleteItemCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenRMS.Contexts.ItemManagement.ApplicationService.CommandStack.Commands;
+
+namespace OpenRMS.Contexts.ItemManagement.ApplicationService.Tests.CommandStack.Commands
+{
+    [TestClass]
+    public class DeleteItemCommandTests
+    {
+        [TestMethod]
+        public void Id_AfterClassInstantiation_ReturnsCorrectValue()
+        {
+            // ARRANGE
+            var id = Guid.NewGuid();
+
+            // ACT
+            var command = new DeleteItemCommand(id);
+
+            // ASSERT
+            command.Id.Should().Be(id);
+        }
+    }
+}

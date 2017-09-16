32489a78884d0dd3a709db8a78aa32e0343b13d6
Duane Wingett <dibley1973@yahoo.co.uk>
2017-09-16 10:05:04 +0100

    Added basic test and fake to SharedKernel unit tests. VS does not discover test yet, so more work to do before adding further tests. #37

---
+++ Code/Shared/ORMS.Shared.SharedKernel.UnitTests/Fakes/FakeProduct.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FakeProduct.cs" company="Chesil Media">
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ORMS.Shared.SharedKernel.UnitTests.Fakes
+{
+    using BaseClasses;
+
+    /// <summary>
+    /// Represents a fake product for testoing purposes only
+    /// </summary>
+    public class FakeProduct : Entity<int>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeProduct"/> class.
+        /// </summary>
+        public FakeProduct()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeProduct"/> class.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        public FakeProduct(int id)
+            : base(id)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+    }
+}
---
+++ Code/Shared/ORMS.Shared.SharedKernel.UnitTests/Tests/BaseClasses/EntityTests.cs
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EntityTests.cs" company="Chesil Media">
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ORMS.Shared.SharedKernel.UnitTests.Tests.BaseClasses
+{
+    using Fakes;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using SharedKernel.BaseClasses;
+
+    /// <summary>
+    /// Provides unit tests for the <see cref="Entity{TId}"/> class.
+    /// </summary>
+    [TestFixture]
+    public class EntityTests
+    {
+        /// <summary>
+        /// Equalses the when other is same type but null returns false.
+        /// </summary>
+        [Test]
+        public void Equals_WhenOtherIsSameTypeButNull_ReturnsFalse()
+        {
+            // ARRANGE
+            var product = new FakeProduct(2);
+
+            // ACT
+            var actual = product.Equals(null);
+
+            // ASSERT
+            actual.Should().BeFalse();
+        }
+    }
+}

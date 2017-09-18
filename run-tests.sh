a95fb885a429b493f7b2b81de48091bdcdb1ff19
Duane Wingett <dibley1973@yahoo.co.uk>
2017-09-18 05:33:17 +0100

    Further tweaks to Travis build scripts - Changes path in run-tetst.sh to use forward slashes #41

--- run-tests.sh
+++ run-tests.sh
@@ -1,1 +1,1 @@
-nunit-console Code\Shared\ORMS.Shared.SharedKernel.UnitTests\bin\Release\netcoreapp2.0\ORMS.Shared.SharedKernel.UnitTests.dll
+nunit-console Code/Shared/ORMS.Shared.SharedKernel.UnitTests/bin/Release/netcoreapp2.0/ORMS.Shared.SharedKernel.UnitTests.dll

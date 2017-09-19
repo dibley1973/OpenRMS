2d5ab690e1f32ec905b4731fd64dc8ccc4badf47
Duane Wingett <dibley1973@yahoo.co.uk>
2017-09-19 20:12:41 +0100

    Added tests for Result.Fail #43

--- Code/Shared/ORMS.Shared.SharedKernel.UnitTests/Tests/Amplifiers/ResultTests/FailedResultTests.cs
+++ Code/Shared/ORMS.Shared.SharedKernel.UnitTests/Tests/Amplifiers/ResultTests/FailedResultTests.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="SuccessfulResultTests.cs" company="Chesil Media">
+// <copyright file="FailedResultTests.cs" company="Chesil Media">
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
@@ -21,75 +21,148 @@ namespace ORMS.Shared.SharedKernel.UnitTests.Tests.Amplifiers.ResultTests
     internal class FailedResultTests
     {
         /// <summary>
-        /// Givens a non generic result when fail then error message should match supplied message.
+        /// Given the Error property when a non generic result fail then message should match supplied message.
         /// </summary>
         [Test]
-        public void GivenANonGenericResult_WhenFail_ThenErrorMessageShouldMatchSuppliedMessage()
+        public void GivenError_WhenANonGenericResultFail_ThenMessageShouldMatchSuppliedMessage()
         {
             // ARRANGE
-            Result result = Result.Fail("Error message");
+            var result = Result.Fail("Error message");
 
             // ASSERT
             result.Error.Should().Be("Error message");
         }
 
         /// <summary>
-        /// Givens a non generic result when fail then failure should be true.
+        /// Given the isFailure property when a non generic result fail then should be true.
         /// </summary>
         [Test]
-        public void GivenANonGenericResult_WhenFail_ThenFailureShouldBeTrue()
+        public void GivenIsFailure_WhenANonGenericResultFail_ThenShouldBeTrue()
         {
             // ARRANGE
-            Result result = Result.Fail("Error message");
+            var result = Result.Fail("Error message");
 
             // ASSERT
             result.IsFailure.Should().Be(true);
         }
 
         /// <summary>
-        /// Givens a non generic result when fail then success should be false.
+        /// Given the IsSuccess property when a non generic fail result then should be false.
         /// </summary>
         [Test]
-        public void GivenANonGenericResult_WhenFail_ThenSuccessShouldBeFalse()
+        public void GivenIsSuccess_WhenANonGenericFailResult_ThenShouldBeFalse()
         {
             // ARRANGE
-            Result result = Result.Fail("Error message");
+            var result = Result.Fail("Error message");
 
             // ASSERT
             result.IsSuccess.Should().Be(false);
         }
 
+        /// <summary>
+        /// Given the Error property when a generic result fail then message should match supplied message.
+        /// </summary>
         [Test]
-        public void Can_create_a_generic_version()
+        public void GivenError_WhenAGenericResultFail_ThenMessageShouldMatchSuppliedMessage()
         {
-            Result<FakeProduct> result = Result.Fail<FakeProduct>("Error message");
+            // ARRANGE
+            var result = Result.Fail<FakeProduct>("Error message");
 
+            // ASSERT
             result.Error.Should().Be("Error message");
+        }
+
+        /// <summary>
+        /// Given the isFailure property when a generic result fail then should be true.
+        /// </summary>
+        [Test]
+        public void GivenIsFailure_WhenAGenericResultFail_ThenShouldBeTrue()
+        {
+            // ARRANGE
+            var result = Result.Fail<FakeProduct>("Error message");
+
+            // ASSERT
             result.IsFailure.Should().Be(true);
+        }
+
+        /// <summary>
+        /// Given the IsSuccess property when a generic fail result then should be false.
+        /// </summary>
+        [Test]
+        public void GivenIsSuccess_WhenAGenericFailResult_ThenShouldBeFalse()
+        {
+            // ARRANGE
+            var result = Result.Fail<FakeProduct>("Error message");
+
+            // ASSERT
             result.IsSuccess.Should().Be(false);
         }
 
+        /// <summary>
+        /// Given the value property when a genericfail result then throws exception.
+        /// </summary>
         [Test]
-        public void Cannot_access_Value_property()
+        public void GivenValueProperty_WhenAGenericfailResult_ThenThrowsException()
         {
-            Result<FakeProduct> result = Result.Fail<FakeProduct>("Error message");
+            // ARRANGE
+            var result = Result.Fail<FakeProduct>("Error message");
 
+            // ACT
             Action action = () => { FakeProduct myClass = result.Value; };
 
+            // ASSERT
             action.ShouldThrow<InvalidOperationException>();
         }
 
+        /// <summary>
+        /// Given a non generic fail creation when null message throws exception.
+        /// </summary>
         [Test]
-        public void Cannot_create_without_error_message()
+        public void GivenANonGenericFailCreation_WhenNullMessage_ThrowsException()
         {
+            // ARRANGE
             Action action1 = () => { Result.Fail(null); };
+
+            // ASSERT
+            action1.ShouldThrow<ArgumentNullException>();
+        }
+
+        /// <summary>
+        /// Given a non generic fail creation when empty string message throws exception.
+        /// </summary>
+        [Test]
+        public void GivenANonGenericFailCreation_WhenEmptyStringMessage_ThrowsException()
+        {
+            // ARRANGE
             Action action2 = () => { Result.Fail(string.Empty); };
+
+            // ASSERT
+            action2.ShouldThrow<ArgumentNullException>();
+        }
+
+        /// <summary>
+        /// Given a generic fail creation when null message throws exception.
+        /// </summary>
+        [Test]
+        public void GivenAGenericFailCreation_WhenNullMessage_ThrowsException()
+        {
+            // ARRANGE
             Action action3 = () => { Result.Fail<FakeProduct>(null); };
+
+            // ASSERT
+            action3.ShouldThrow<ArgumentNullException>();
+        }
+
+        /// <summary>
+        /// Given a generic fail creation when empty string message throws exception.
+        /// </summary>
+        [Test]
+        public void GivenAGenericFailCreation_WhenEmptyStringMessage_ThrowsException()
+        {
+            // ARRANGE
             Action action4 = () => { Result.Fail<FakeProduct>(string.Empty); };
 
-            action1.ShouldThrow<ArgumentNullException>();
-            action2.ShouldThrow<ArgumentNullException>();
-            action3.ShouldThrow<ArgumentNullException>();
+            // ASSERT
             action4.ShouldThrow<ArgumentNullException>();
         }
     }

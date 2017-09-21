10ec96510a4d416807d52b2c1be359f9433c611c
Duane Wingett <dibley1973@yahoo.co.uk>
2017-09-21 18:15:10 +0100

    Addes `ShortDescription` and tests. #46

---
+++ Code/Shared/ORMS.Shared.SharedKernel.UnitTests/Tests/CommonEntities/ShortDescriptionTests.cs
@@ -0,0 +1,235 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ShortDescriptionTests.cs" company="Chesil Media">
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ORMS.Shared.SharedKernel.UnitTests.Tests.CommonEntities
+{
+    using System;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using ORMS.Shared.SharedKernel.CommonEntities;
+
+    /// <summary>
+    /// Test for <see cref="ShortDescription"/> structure
+    /// </summary>
+    [TestFixture]
+    public class ShortDescriptionTests
+    {
+        /// <summary>
+        /// Given the constructor when called with null value throws exception.
+        /// </summary>
+        [Test]
+        public void GivenConstructor_WhenCalledWithNullValue_ThenThrowsException()
+        {
+            // ARRANGE
+
+            // ACT
+            Action actual = () => new ShortDescription(null);
+
+            // ASSERT
+            actual.ShouldThrow<ArgumentNullException>();
+        }
+
+        /// <summary>
+        /// Given the constructor when called with empty string throws exception.
+        /// </summary>
+        [Test]
+        public void GivenConstructor_WhenCalledWithEmptyValue_ThenThrowsException()
+        {
+            // ARRANGE
+
+            // ACT
+            Action actual = () => new ShortDescription(string.Empty);
+
+            // ASSERT
+            actual.ShouldThrow<ArgumentNullException>();
+        }
+
+        /// <summary>
+        /// Given the constructor when called with white space throws exception.
+        /// </summary>
+        [Test]
+        public void GivenConstructor_WhenCalledWithWhiteSpace_ThenThrowsException()
+        {
+            // ARRANGE
+
+            // ACT
+            Action actual = () => new ShortDescription("  ");
+
+            // ASSERT
+            actual.ShouldThrow<ArgumentNullException>();
+        }
+
+        /// <summary>
+        /// Given the constructor when called with value longer than maximum length throws exception.
+        /// </summary>
+        [Test]
+        public void GivenConstructor_WhenCalledWithValueLongerThanMaximumLength_ThenThrowsException()
+        {
+            // ARRANGE
+            var value = new string('A', ShortDescription.MaximumCharacterLength + 1);
+
+            // ACT
+            Action actual = () => new ShortDescription(value);
+
+            // ASSERT
+            actual.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        /// <summary>
+        /// Given the constructor when called with value longer at maximum length throws exception.
+        /// </summary>
+        [Test]
+        public void GivenConstructor_WhenCalledWithValueAtMaximumLength_ThenThrowsException()
+        {
+            // ARRANGE
+            var value = new string('A', ShortDescription.MaximumCharacterLength);
+
+            // ACT
+            Action actual = () => new ShortDescription(value);
+
+            // ASSERT
+            actual.ShouldNotThrow();
+        }
+
+        /// <summary>
+        /// Given the constructor when called with value less than maximum length throws exception.
+        /// </summary>
+        [Test]
+        public void GivenConstructor_WhenCalledWithValueLessThanMaximumLength_ThenThrowsException()
+        {
+            // ARRANGE
+            var value = new string('A', ShortDescription.MaximumCharacterLength - 1);
+
+            // ACT
+            Action actual = () => new ShortDescription(value);
+
+            // ASSERT
+            actual.ShouldNotThrow();
+        }
+
+        /// <summary>
+        /// Given the value when read after valid construction then returns constructed value.
+        /// </summary>
+        [Test]
+        public void GivenValue_WhenReadAfterValidConstruction_ThenReturnsConstructedValue()
+        {
+            // ARRANGE
+            var value = new string('A', ShortDescription.MaximumCharacterLength - 1);
+            var name = new ShortDescription(value);
+
+            // ACT
+            var actual = name.Value;
+
+            // ASSERT
+            actual.Should().Be(value);
+        }
+
+        /// <summary>
+        /// Given the equals method, when same value strings then returns true.
+        /// </summary>
+        [Test]
+        public void GivenEquals_WhenSameValueStrings_ThenReturnsTrue()
+        {
+            // ARRANGE
+            var name1 = new ShortDescription("ShortDescription");
+            var name2 = new ShortDescription("ShortDescription");
+
+            // ACT
+            var actual = name1.Equals(name2);
+
+            // ASSERT
+            actual.Should().BeTrue();
+        }
+
+        /// <summary>
+        /// Given the equals method, when different value strings then returns false.
+        /// </summary>
+        [Test]
+        public void GivenEquals_WhenDifferentValueStrings_ThenReturnsFalse()
+        {
+            // ARRANGE
+            var name1 = new ShortDescription("ShortDescription1");
+            var name2 = new ShortDescription("ShortDescription2");
+
+            // ACT
+            var actual = name1.Equals(name2);
+
+            // ASSERT
+            actual.Should().BeFalse();
+        }
+
+        /// <summary>
+        /// Givens the get hash code, when same value strings then returns true.
+        /// </summary>
+        [Test]
+        public void GivenGetHashCode_WhenSameValueStrings_ThenReturnsTrue()
+        {
+            // ARRANGE
+            var name1 = new ShortDescription("ShortDescription");
+            var name2 = new ShortDescription("ShortDescription");
+
+            // ACT
+            var actual = name1.GetHashCode().Equals(name2.GetHashCode());
+
+            // ASSERT
+            actual.Should().BeTrue();
+        }
+
+        /// <summary>
+        /// Givens the get hash code, when different value strings then returns false.
+        /// </summary>
+        [Test]
+        public void GivenGetHashCode_WhenDifferentValueStrings_ThenReturnsFalse()
+        {
+            // ARRANGE
+            var name1 = new ShortDescription("ShortDescription1");
+            var name2 = new ShortDescription("ShortDescription2");
+
+            // ACT
+            var actual = name1.GetHashCode().Equals(name2.GetHashCode());
+
+            // ASSERT
+            actual.Should().BeFalse();
+        }
+
+        /// <summary>
+        /// Given the explicit string operator when cast from string then created name with same value.
+        /// </summary>
+        [Test]
+        public void GivenExplicitStringOperator_WhenCastFromString_ThenCreatedShortDescriptionWithSameValue()
+        {
+            // ARRANGE
+            var value = "ShortDescription";
+            ////var name = default(ShortDescription);
+
+            // ACT
+            var actual = (ShortDescription)value;
+
+            // ASSERT
+            actual.Value.Should().Be(value);
+        }
+
+        /// <summary>
+        /// Given the implicit string operator when cast to string then created name with same value.
+        /// </summary>
+        [Test]
+        public void GivenImplicitStringOperator_WhenCastToString_ThenCreatedShortDescriptionWithSameValue()
+        {
+            // ARRANGE
+            var value = "ShortDescription";
+            var name = new ShortDescription(value);
+
+            // ACT
+            string actual = name;
+
+            // ASSERT
+            actual.Should().Be(value);
+        }
+    }
+}
---
+++ Code/Shared/ORMS.Shared.SharedKernel/CommonEntities/ShortDescription.cs
@@ -0,0 +1,104 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ShortDescription.cs" company="Chesil Media">
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ORMS.Shared.SharedKernel.CommonEntities
+{
+    using System;
+    using BaseClasses;
+
+    /// <summary>
+    /// Represents a short description
+    /// </summary>
+    /// <seealso cref="ValueObject{ShortDescription}" />
+    public class ShortDescription : ValueObject<ShortDescription>
+    {
+        /// <summary>
+        /// The maximum number of characters this instance can be.
+        /// </summary>
+        public const byte MaximumCharacterLength = 255;
+
+        private readonly string _value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShortDescription" /> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ArgumentNullException">Thrown if value is null, empty or white space.</exception>
+        public ShortDescription(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException(nameof(value));
+            if (value.Length > MaximumCharacterLength) throw new ArgumentOutOfRangeException(nameof(value), $"Must not exceed {MaximumCharacterLength} characters in length");
+
+            _value = value;
+        }
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="ShortDescription"/> class from being created.
+        /// </summary>
+        private ShortDescription()
+        {
+        }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public string Value => _value;
+
+        /// <summary>
+        /// Performs an explicit conversion from <see cref="string"/> to <see cref="ShortDescription"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
+        public static explicit operator ShortDescription(string value)
+        {
+            return new ShortDescription(value);
+        }
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="string" /> to <see cref="ShortDescription" />.
+        /// </summary>
+        /// <param name="description">The description.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
+        public static implicit operator string(ShortDescription description)
+        {
+            return description.Value;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="T:System.Object" />, is equal to this instance
+        /// of <see cref="T:ORMS.Shared.SharedKernel.BaseClasses.ValueObject`1" />.
+        /// </summary>
+        /// <param name="other">The other.</param>
+        /// <returns>
+        /// <c>true</c> if the specified <see cref="T:ORMS.Shared.SharedKernel.BaseClasses.ValueObject`1" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        protected override bool EqualsCore(ShortDescription other)
+        {
+            return Value == other.Value;
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// Returns a hash code for this instance
+        /// </returns>
+        protected override int GetHashCodeCore()
+        {
+            return GetType().ToString().GetHashCode() * Value.GetHashCode() ^ 307;
+        }
+    }
+}

9a6e4bbd2b5491cbba969fc8e01ca4b5b4edcfe1
Duane Wingett <dibley1973@yahoo.co.uk>
2017-09-26 06:20:03 +0100

    Added Location Management project and also `Location` and `LocationState` classes, but no tests to cover, as yet.

---
+++ Code/LocationManagement/OpenRMS.Contexts.LocationManagement.Domain/Constants/ResultErrorKeys/LocationState.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LocationState.cs" company="Chesil Media">
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace OpenRMS.Contexts.LocationManagement.Domain.Constants.ResultErrorKeys
+{
+    using ORMS.Shared.SharedKernel.BaseClasses;
+
+    /// <summary>
+    /// Represents the state of an <see cref="Location"/>
+    /// </summary>
+    public class LocationState : Entity<int>
+    {
+        /// <summary>
+        /// Creates an instance of an <see cref="LocationState"/> which represents the "created" state.
+        /// </summary>
+        public static readonly LocationState Created = new LocationState(1, "Created");
+
+        /// <summary>
+        /// Creates an instance of an <see cref="LocationState"/> which represents the "Active" state.
+        /// </summary>
+        public static readonly LocationState Active = new LocationState(2, "Active");
+
+        /// <summary>
+        /// Creates an instance of an <see cref="LocationState"/> which represents the "Deactivated" state.
+        /// </summary>
+        public static readonly LocationState Deactivated = new LocationState(3, "Deactivated");
+
+        private readonly string _name;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocationState"/> class.
+        /// </summary>
+        /// <param name="id">The identity of the state</param>
+        /// <param name="name">The name of the state</param>
+        private LocationState(int id, string name)
+            : base(id)
+        {
+            _name = name;
+        }
+
+        /// <summary>
+        /// Gets a value for the name
+        /// </summary>
+        public string Name => _name;
+    }
+}
